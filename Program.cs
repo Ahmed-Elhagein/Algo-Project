03a73c3a35f38be032c952d63e078496e51ede3b
Ahmed-Elhagein <ahmedelhagein123@gmail.com>
2025-12-25 01:47:16 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LongestRepeatedSubstring_Optimized_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
